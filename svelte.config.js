bd8d6956e2a29aa1faf255996f7355ad46914e70
Tomáš Písecký <tomas.pisecky@avast.com>
2022-07-27 11:09:15 +0200

    vite

--- svelte.config.js
+++ svelte.config.js
@@ -4,14 +4,8 @@ import preprocess from 'svelte-preprocess';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
-		// adapter: adapter(),
 
 		adapter: adapter(),
-
-		// Override http methods in the Todo forms
-		methodOverride: {
-			allowed: ['PATCH', 'DELETE']
-		},
 
 		prerender: {
 			crawl: true,
